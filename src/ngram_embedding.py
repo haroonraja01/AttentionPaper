73c64854a2e763807000f816ad6b4cbbe7679957
Haroon Raja <haroonraja86@gmail.com>
2025-01-27 20:55:04 -0500

    embeddings

--- src/ngram_embedding.py
+++ src/ngram_embedding.py
@@ -1,40 +1,49 @@
-# This script contains some examples of word embeddings
+# Author: Robert Guthrie
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
 torch.manual_seed(1)
-
-
-CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
+CONTEXT_SIZE = 2
 EMBEDDING_DIM = 10
-raw_text = """We are about to study the idea of a computational process.
-Computational processes are abstract beings that inhabit computers.
-As they evolve, processes manipulate other abstract things called data.
-The evolution of a process is directed by a pattern of rules
-called a program. People create programs to direct processes. In effect,
-we conjure the spirits of the computer with our spells.""".split()
-
-# By deriving a set from `raw_text`, we deduplicate the array
-vocab = set(raw_text)
-vocab_size = len(vocab)
-
-word_to_ix = {word: i for i, word in enumerate(vocab)}
-data = []
-for i in range(CONTEXT_SIZE, len(raw_text) - CONTEXT_SIZE):
-    context = (
-        [raw_text[i - j - 1] for j in range(CONTEXT_SIZE)]
-        + [raw_text[i + j + 1] for j in range(CONTEXT_SIZE)]
+# We will use Shakespeare Sonnet 2
+test_sentence = """When forty winters shall besiege thy brow,
+And dig deep trenches in thy beauty's field,
+Thy youth's proud livery so gazed on now,
+Will be a totter'd weed of small worth held:
+Then being asked, where all thy beauty lies,
+Where all the treasure of thy lusty days;
+To say, within thine own deep sunken eyes,
+Were an all-eating shame, and thriftless praise.
+How much more praise deserv'd thy beauty's use,
+If thou couldst answer 'This fair child of mine
+Shall sum my count, and make my old excuse,'
+Proving his beauty by succession thine!
+This were to be new made when thou art old,
+And see thy blood warm when thou feel'st it cold.""".split()
+# we should tokenize the input, but we will ignore that for now
+# build a list of tuples.
+# Each tuple is ([ word_i-CONTEXT_SIZE, ..., word_i-1 ], target word)
+ngrams = [
+    (
+        [test_sentence[i - j - 1] for j in range(CONTEXT_SIZE)],
+        test_sentence[i]
     )
-    target = raw_text[i]
-    data.append((context, target))
-print(data[:5])
+    for i in range(CONTEXT_SIZE, len(test_sentence))
+]
+# Print the first 3, just so you can see what they look like.
+print(ngrams[:3])
 
-class CBOW(nn.Module):
+vocab = set(test_sentence)
+word_to_ix = {word: i for i, word in enumerate(vocab)}
+
+
+class NGramLanguageModeler(nn.Module):
 
     def __init__(self, vocab_size, embedding_dim, context_size):
-        super(CBOW, self).__init__()
+        super(NGramLanguageModeler, self).__init__()
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
         self.linear1 = nn.Linear(context_size * embedding_dim, 128)
         self.linear2 = nn.Linear(128, vocab_size)
@@ -46,30 +55,19 @@ class CBOW(nn.Module):
         log_probs = F.log_softmax(out, dim=1)
         return log_probs
 
-# Create your model and train. Here are some functions to help you make
-# the data ready for use by your module.
-
-
-def make_context_vector(context, word_to_ix):
-    idxs = [word_to_ix[w] for w in context]
-    return torch.tensor(idxs, dtype=torch.long)
-
-
-words_to_train = make_context_vector(data[0][0], word_to_ix)  # example
-
 
 losses = []
 loss_function = nn.NLLLoss()
-model = CBOW(len(vocab), EMBEDDING_DIM, 2 * CONTEXT_SIZE)
+model = NGramLanguageModeler(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE)
 optimizer = optim.SGD(model.parameters(), lr=0.001)
+
 for epoch in range(10):
     total_loss = 0
-    for context, target in data:
-        # print(context, target)
+    for context, target in ngrams:
 
         # Step 1. Prepare the inputs to be passed to the model (i.e, turn the words
         # into integer indices and wrap them in tensors)
-        context_idxs = make_context_vector(context, word_to_ix)
+        context_idxs = torch.tensor([word_to_ix[w] for w in context], dtype=torch.long)
 
         # Step 2. Recall that torch *accumulates* gradients. Before passing in a
         # new instance, you need to zero out the gradients from the old
@@ -94,4 +92,4 @@ for epoch in range(10):
 print(losses)  # The loss decreased every iteration over the training data!
 
 # To get the embedding of a particular word, e.g. "beauty"
-print(model.embeddings.weight[word_to_ix["about"]])
+print(model.embeddings.weight[word_to_ix["beauty"]])
